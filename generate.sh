b9676bc692664192b1b1e5e6ebb8b2d1b5547425
Alessandro Chiarotto <alessandro.chiarotto.dechit@skytv.it>
2024-01-11 12:23:27 +0100

    ..

--- generate.sh
+++ generate.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
-OUTPUT_DIR="./samples/client/coface"
+SOURCE_OUTPUT_DIR="./samples/client/coface"
+DOC_OUTPUT_DIR="./samples/docs/client/coface"
+rm -rf SOURCE_OUTPUT_DIR
+rm -rf DOC_OUTPUT_DIR
 
-rm -rf samples/client/coface
+# Generate source code
 java -jar modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate \
   -i ./open-api/coface.yaml \
   -l kotlin-server \
-  -o "${OUTPUT_DIR}" \
-  -t templates/kotlin-client
+  -o "${SOURCE_OUTPUT_DIR}" \
+  -t templates/kotlin-client \
+
+
+# Generate doc
+java -jar modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate \
+  -i ./open-api/coface.yaml \
+  -l html \
+  -o "${DOC_OUTPUT_DIR}" \
+
